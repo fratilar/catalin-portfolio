a4764cdd2c1c2e7a761fe7abf8fbbf254d3f1eb3
Razvan Fratila <fratila_razvan24@yahoo.com>
2022-04-21 09:48:07 +0300

    added new photos

--- projects.js
+++ projects.js
@@ -1,62 +1,67 @@
 export const projects = [
-   {
-      id: 1,
-      title: "Mind of a killer",
-      src: "/Images/01.png",
-   },
-   {
-      id: 2,
-      title: "Meta Kid",
-      src: "/Images/02.png",
-   },
-   {
-      id: 3,
-      title: "Young Blood",
-      src: "/Images/03.png",
-   },
-   {
-      id: 4,
-      title: "Free Will",
-      src: "/Images/04.png",
-   },
-   {
-      id: 5,
-      title: "Too much love",
-      src: "/Images/05.png",
-   },
-   {
-      id: 6,
-      title: "Be different",
-      src: "/Images/06.png",
-   },
-   {
-      id: 7,
-      title: "Boys will be boys",
-      src: "/Images/07.png",
-   },
-   {
-      id: 8,
-      title: "Partners in crime-boy",
-      src: "/Images/08.png",
-   },
-   {
-      id: 9,
-      title: "Partners in crime-girl",
-      src: "/Images/09.png",
-   },
-   {
-      id: 10,
-      title: "Always Cooking",
-      src: "/Images/10.png",
-   },
-   {
-      id: 11,
-      title: "Wild Child",
-      src: "/Images/11.png",
-   },
-   {
-      id: 12,
-      title: "Intermission",
-      src: "/Images/12.png",
-   },
+  {
+    id: 1,
+    title: "Mind of a killer",
+    src: "/Images/01.png",
+  },
+  {
+    id: 2,
+    title: "Meta Kid",
+    src: "/Images/02.png",
+  },
+  {
+    id: 3,
+    title: "Young Blood",
+    src: "/Images/03.png",
+  },
+  {
+    id: 4,
+    title: "Free Will",
+    src: "/Images/04.png",
+  },
+  {
+    id: 5,
+    title: "Too much love",
+    src: "/Images/05.png",
+  },
+  {
+    id: 6,
+    title: "Be different",
+    src: "/Images/06.png",
+  },
+  {
+    id: 7,
+    title: "Boys will be boys",
+    src: "/Images/07.png",
+  },
+  {
+    id: 8,
+    title: "Partners in crime-boy",
+    src: "/Images/08.png",
+  },
+  {
+    id: 9,
+    title: "Partners in crime-girl",
+    src: "/Images/09.png",
+  },
+  {
+    id: 10,
+    title: "Always Cooking",
+    src: "/Images/10.png",
+  },
+  {
+    id: 11,
+    title: "Wild Child",
+    src: "/Images/11.png",
+  },
+  {
+    id: 12,
+    title: "Intermission",
+    src: "/Images/12.png",
+  },
+  {
+    id: 13,
+    title: "Summer Love",
+    src: "/Images/13.png",
+  },
 ];
